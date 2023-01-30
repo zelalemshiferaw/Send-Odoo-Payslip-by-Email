4d3fca7083b11392640513884d461b710c6df117
zelalem shiferaw <zelalemshiferaw71921@gmail.com>
2023-01-30 23:17:26 +0300

    Payroll Email Odoo

---
+++ payroll_email/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Payroll Email/Mass E-mail',
+    'description': 'Send payslip through Email.',
+    'category': 'Generic Modules/Human Resources',
+    'depends': ['base', 'hr_payroll', 'mail', 'hr'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/mail_template.xml',
+        'views/hr_payroll.xml',
+        'views/hr_payslip_wizard_view.xml',
+        'views/hr_mass_payroll_wizard.xml'
+        
+    ],
+    'demo': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ payroll_email/models/__init__.py
@@ -0,0 +1,4 @@
+from . import hr_payroll
+from . import hr_payroll_massmail
+
+
